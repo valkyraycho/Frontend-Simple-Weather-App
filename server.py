9eedbc33526083f58ffa0eb795915b2f52f76a82
Ray Cho <razor0424@gmail.com>
2023-11-29 07:41:39 -0800

    First commit

---
+++ server.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+from weather import get_current_weather
+from waitress import serve
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/weather')
+def get_weather():
+    city = request.args.get('city') # from name=city in the input tag from the templates
+    
+    if not bool(city.strip()):
+        return render_template('city-not-found.html')
+        
+    weather_data = get_current_weather(city)
+    
+    if not weather_data['cod'] == 200:
+        return render_template('city-not-found.html')
+    
+    return render_template('weather.html',
+                           title=weather_data['name'],
+                           status=weather_data['weather'][0]['description'],
+                           temp=f"{weather_data['main']['temp']:.1f}",
+                           feels_like=f"{weather_data['main']['feels_like']:.1f}")
+
+if __name__ == '__main__':
+    serve(app, host='0.0.0.0', port=8000)
